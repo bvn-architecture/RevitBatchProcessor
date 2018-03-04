3404b70b917ae1770bcd19857c2ebaa2383abbcb
Dan Rumery <Dan_Rumery@bvn.com.au>
2018-03-04 15:14:19 +1100

    BatchRvt: Scripts: revit_dynamo_util.py: check the RunType (Run mode) of the Dynamo script. If the RunType is not set to 'Automatic' then raise an error (and do not attempt to execute the Dynamo script).

--- BatchRvt/Scripts/revit_dynamo_util.py
+++ BatchRvt/Scripts/revit_dynamo_util.py
@@ -2,14 +2,39 @@
 import clr
 import System
 
+clr.AddReference("System.Xml")
+from System.Xml import XmlDocument
+
 clr.AddReference("DynamoRevitDS")
 from Dynamo.Applications import DynamoRevit, DynamoRevitCommandData
+
+DYNAMO_RUNTYPE_AUTOMATIC = "Automatic"
+DYNAMO_RUNTYPE_MANUAL = "Manual"
+DYNAMO_WORKSPACE_NODE = "Workspace"
+DYNAMO_RUNTYPE_ATTRIBUTE = "RunType"
+
+def GetDynamoScriptRunType(dynamoScriptFilePath):
+  runType = None
+  doc = XmlDocument()
+  try:
+    doc.Load(dynamoScriptFilePath)
+    runType = doc[DYNAMO_WORKSPACE_NODE].Attributes[DYNAMO_RUNTYPE_ATTRIBUTE].Value
+  except Exception, e:
+    runType = None
+  return runType
 
 # NOTE: Dynamo requires an active UIDocument! The document must be active before executing this function.
 #       The Dynamo script must have been saved with the 'Automatic' run mode!
 def ExecuteDynamoScript(uiapp, dynamoScriptFilePath, showUI=False):
+  dynamoScriptRunType = GetDynamoScriptRunType(dynamoScriptFilePath)
+  if dynamoScriptRunType is None:
+    raise Exception("Could not determine the Run mode of this Dynamo script!")
+  elif dynamoScriptRunType != DYNAMO_RUNTYPE_AUTOMATIC:
+    raise Exception(
+        "The Dynamo script has Run mode set to '" + dynamoScriptRunType + "'. " +
+        "It must be set to '" + DYNAMO_RUNTYPE_AUTOMATIC + "' in order for Dynamo script automation to work."
+      )
   revitVersionNumber = uiapp.Application.VersionNumber
-
   if revitVersionNumber == "2015":
     raise Exception("Automation of Dynamo scripts is not supported in Revit 2015!")
   elif revitVersionNumber == "2016":
