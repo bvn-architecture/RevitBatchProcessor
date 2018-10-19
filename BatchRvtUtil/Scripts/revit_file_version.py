45bc88f3a176018b604569506f6f64303ee03618
Dan Rumery <Dan_Rumery@bvn.com.au>
2018-10-19 16:47:36 +1100

    BatchRvtUtil: Scripts: revit_file_version.py: new method of extracting Revit file version information (accounts for Revit 2019 files and other variations on Revit file version information structure). (WIP)

--- BatchRvtUtil/Scripts/revit_file_version.py
+++ BatchRvtUtil/Scripts/revit_file_version.py
@@ -28,12 +28,17 @@ from System.Linq import Enumerable
 import System.Reflection as Refl
 
 import System.IO
+from System.IO import IOException
+
+from System.Reflection import TargetInvocationException
 
 import System.Text
 from System.Text import Encoding
 
 clr.AddReference("WindowsBase")
 import System.IO.Packaging as Packaging
+
+import util
 
 STORAGE_ROOT_TYPE_NAME = "System.IO.Packaging.StorageRoot"
 STORAGE_ROOT_OPEN_METHOD_NAME = "Open"
@@ -88,4 +93,83 @@ def GetRevitVersionText(revitFilePath):
   end = unicodeString.IndexOf("\x00", start)
   versionText = unicodeString.Substring(start, end - start)
   return versionText.Substring(0, versionText.LastIndexOf(")") + 1) 
+
+def GetBasicFileInfoBytes(revitFilePath):
+  storageRoot = GetStorageRoot(revitFilePath)
+  stream = GetBasicFileInfoStream(storageRoot)
+  bytes = ReadAllBytes(stream)
+  return bytes
+
+def GetRevitFileVersionInfoText(revitFilePath):
+  revitVersionInfoText = str.Empty
+  bytes = GetBasicFileInfoBytes(revitFilePath)
+  asciiString = Encoding.ASCII.GetString(bytes)
+  TEXT_MARKER = '\r\n'
+  TEXT_MARKER_ALT = '\x04\r\x00\n\x00'
+  textMarker = TEXT_MARKER
+  textMarkerIndices = util.FindAllIndicesOf(asciiString, textMarker)
+  numberOfTextMarkerIndices = len(textMarkerIndices)
+  if numberOfTextMarkerIndices != 2:
+    textMarker = TEXT_MARKER_ALT
+    textMarkerIndices = util.FindAllIndicesOf(asciiString, textMarker)
+    numberOfTextMarkerIndices = len(textMarkerIndices)
+  if numberOfTextMarkerIndices == 2:
+    startTextIndex = textMarkerIndices[0] + len(textMarker)
+    endTextIndex = textMarkerIndices[1]
+    textBytes = bytes[startTextIndex:endTextIndex]
+    revitVersionInfoText = Encoding.Unicode.GetString(bytes[startTextIndex:endTextIndex])
+  return revitVersionInfoText
+
+def TryGetRevitFileVersionInfoText(revitFilePath):
+  revitVersionInfoText = str.Empty
+  try:
+    revitVersionInfoText = GetRevitFileVersionInfoText(revitFilePath)
+  except TargetInvocationException, e:
+    revitVersionInfoText = str.Empty
+  except IOException, e:
+    revitVersionInfoText = str.Empty
+  return revitVersionInfoText
+
+def ExtractRevitVersionInfoFromText(revitVersionInfoText):
+  REVIT_BUILD_PROPERTY = "Revit Build:"
+  FORMAT_PROPERTY = "Format:"
+  BUILD_PROPERTY = "Build:"
+  revitVersionDescription = str.Empty
+  lines = util.ReadLinesFromText(revitVersionInfoText)
+  indexedLines = [[index, line] for index, line in enumerate(lines)]
+  # Revit 2019 (and onwards?) has 'Build' and 'Format' properties instead of 'Revit Build'
+  formatLine = indexedLines.SingleOrDefault(lambda l: l[1].StartsWith(FORMAT_PROPERTY))
+  buildLine = indexedLines.SingleOrDefault(lambda l: l[1].StartsWith(BUILD_PROPERTY))
+  if buildLine is not None:
+    buildLineText = buildLine[1]
+    buildLineText = buildLineText[len(BUILD_PROPERTY):]
+    formatLineText = str.Empty
+    if formatLine is not None:
+      formatLineText = formatLine[1]
+      formatLineText = formatLineText[len(FORMAT_PROPERTY):]
+      revitVersionDescription = "Autodesk Revit " + formatLineText.Trim() + " (Build: " + buildLineText.Trim() + ")"
+  else:
+    revitBuildLine = indexedLines.SingleOrDefault(lambda l: l[1].StartsWith(REVIT_BUILD_PROPERTY))
+    revitBuildLineText = str.Empty
+    if revitBuildLine is None:
+      # In rare cases the Revit Build *value* is on the next line for some reason!
+      # In this scenario it seems to always be followed immediately (no spaces) by the 'Last Save Path:' property specifier
+      revitBuildLine = indexedLines.SingleOrDefault(lambda l: l[1].Contains(REVIT_BUILD_PROPERTY))
+      if revitBuildLine is not None:
+        lineNumber = revitBuildLine[0]
+        revitBuildLine = indexedLines[lineNumber+1]
+        revitBuildLineText = revitBuildLine[1]
+        indexOfLastSavePath = revitBuildLineText.IndexOf("Last Save Path:")
+        revitBuildLineText = revitBuildLineText[:indexOfLastSavePath] if indexOfLastSavePath != -1 else revitBuildLineText
+    else:
+      revitBuildLineText = revitBuildLine[1]
+      revitBuildLineText = revitBuildLineText[len(REVIT_BUILD_PROPERTY):]
+    revitVersionDescription = revitBuildLineText.Trim()
+  return revitVersionDescription
+
+def ExtractRevitVersionDescription(revitFilePath):
+  revitVersionInfoText = TryGetRevitFileVersionInfoText(revitFilePath)
+  revitVersionDescription = ExtractRevitVersionInfoFromText(revitVersionInfoText)
+  return revitVersionDescription
+
 
