7da08321c61078d7ebc4c1d88eb63c9a1c4486f2
Dan Rumery <Dan_Rumery@bvn.com.au>
2019-09-12 23:20:18 +1000

    Setup: inno_setup_compile.bat: use Inno Setup version 6 (if available, fallback to version 5 otherwise)

--- Setup/inno_setup_compile.bat
+++ Setup/inno_setup_compile.bat
@@ -1,12 +1,17 @@
 @echo off
-set InnoSetupFolderPathA="C:\Program Files (x86)\Inno Setup 5"
+set InnoSetupFolderPathA="C:\Program Files (x86)\Inno Setup 6"
+set InnoSetupFolderPathB="C:\Program Files (x86)\Inno Setup 5"
 
 if EXIST %InnoSetupFolderPathA% (
   set InnoSetupFolderPath=%InnoSetupFolderPathA%
 ) else (
-  echo.
-  echo ERROR: could not locate an Inno Setup 5 installation folder!
-  exit /b 1
+  if EXIST %InnoSetupFolderPathB% (
+    set InnoSetupFolderPath=%InnoSetupFolderPathB%
+  ) else (
+    echo.
+    echo ERROR: could not locate an Inno Setup 5 or 6 installation folder!
+    exit /b 1
+  )
 )
 
 set InnoSetupCompileOptions=
